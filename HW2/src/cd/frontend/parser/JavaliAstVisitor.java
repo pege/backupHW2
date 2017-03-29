907644c49b40828e71b95bc0d075a39607e4f2f0
panubala <panuyabalasuntharam@panuyas-mbp.home>
2017-03-29 21:32:38 +0200

    NewMethod and NewArray

--- HW2/src/cd/frontend/parser/JavaliAstVisitor.java
+++ HW2/src/cd/frontend/parser/JavaliAstVisitor.java
@@ -47,7 +47,10 @@ import cd.frontend.parser.JavaliParser.MethodDeclContext;
 import cd.frontend.parser.JavaliParser.NEGexprContext;
 import cd.frontend.parser.JavaliParser.NOTexprContext;
 import cd.frontend.parser.JavaliParser.NULLlitContext;
+import cd.frontend.parser.JavaliParser.NewArrayContext;
 import cd.frontend.parser.JavaliParser.NewExprContext;
+import cd.frontend.parser.JavaliParser.NewMethodContext;
+import cd.frontend.parser.JavaliParser.NewTypeContext;
 import cd.frontend.parser.JavaliParser.ORexprContext;
 import cd.frontend.parser.JavaliParser.PARexprContext;
 import cd.frontend.parser.JavaliParser.POSexprContext;
@@ -121,12 +124,34 @@ public final class JavaliAstVisitor extends JavaliBaseVisitor<List<Ast>> {
 		return astList;
 	}
 
-	// TODO implement
+
+
 	@Override
-	public List<Ast> visitNewExpr(NewExprContext ctx) {
-		System.out.println("==New Expr");
+	public List<Ast> visitNewType(NewTypeContext ctx) {
+		// TODO Auto-generated method stub
+		return super.visitNewType(ctx);
+	}
+
+	@Override
+	public List<Ast> visitNewMethod(NewMethodContext ctx) {
+		ArrayList<Ast> astList = new ArrayList<Ast>();
 		
-		return null;
+		String typeName = ctx.Ident().toString();
+		
+		astList.add(new Ast.NewObject(typeName));
+		return astList;
+	}
+
+	@Override
+	public List<Ast> visitNewArray(NewArrayContext ctx) {
+		ArrayList<Ast> astList = new ArrayList<Ast>();
+		
+		String typeName = ctx.Ident().toString() + " []";
+		Ast.Expr capacity = (Ast.Expr) visit(ctx.expr()).get(0);
+		
+		astList.add(new Ast.NewArray(typeName, capacity));
+		
+		return super.visitNewArray(ctx);
 	}
 
 	// not tested
@@ -681,7 +706,7 @@ public final class JavaliAstVisitor extends JavaliBaseVisitor<List<Ast>> {
 	// PrimitiveType
 	@Override
 	public List<Ast> visitPrimitiveType(PrimitiveTypeContext ctx) {
-		System.out.println("==PrimitiveType"); //TODO what is the different between PrimType and PrimitiveType?
+		System.out.println("==PrimitiveType"); 
 		ArrayList<Ast> astList = new ArrayList<>();
 		
 		String type = ctx.getText();
@@ -708,7 +733,9 @@ public final class JavaliAstVisitor extends JavaliBaseVisitor<List<Ast>> {
 	public List<Ast> visitArrayTypeIdent(ArrayTypeIdentContext ctx) {
 		System.out.println("==ArrayTypeIdent");
 		ArrayList<Ast> astList = new ArrayList<>();
-		astList.add(new Ast.VarDecl(null, null));
+		
+		String type = ctx.Ident().toString() + " []";
+		astList.add(new Ast.VarDecl(type, null));
 		return astList;
 	}
 
